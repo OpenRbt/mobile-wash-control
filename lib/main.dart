88e88b714d0353ee919f5bd818ced9f68b3f778b
Darkhan <halykmb@UEP-00055674-MB.local>
2025-12-19 15:27:08 +0500

    fix: repository test

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,26 @@ import 'dart:core';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_wash_control/application/Application.dart';
-import 'package:mobile_wash_control/generated/generated/codegen_loader.g.dart';
 
+import 'generated/codegen_loader.g.dart';
 
 final RouteObserver<PageRoute> routeObserver = new RouteObserver<PageRoute>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  EasyLocalization.logger.enableLevels = [];
-
   runApp(
     EasyLocalization(
-      supportedLocales: const [Locale('ru'), Locale('en')],
+      supportedLocales: const [
+        Locale('ru'),
+        Locale('en'),
+      ],
+      startLocale: Locale('ru'),
       path: 'assets/translations',
-      fallbackLocale: const Locale('ru'),
+      fallbackLocale: Locale('ru'),
       assetLoader: const CodegenLoader(),
       child: Application(),
     ),
   );
+
 }
